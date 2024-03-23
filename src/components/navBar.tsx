b74f2e0f3628aa633f3e3a258fd7d2ca83d73dc4
nata-non <natanon.han@gmail.com>
2024-03-23 19:23:22 +0700

    fix: navbar button position

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,24 +1,22 @@
-import Logo from "../../public/assets/logo";
 import Image from "next/image";
 
 export default function NavBar() {
   return (
-    <div className="flex fixed top-0 min-w-full justify-between bg-white py-4 z-50">
-      <div className="flex gap-2 cursor-pointer m-auto">
-        <div>
-          <Image
-            src="/assets/searchIcon.webp"
-            alt="Vercel Logo"
-            width={32}
-            height={32}
-            priority
-          />
+    <div className="flex fixed top-0 min-w-full bg-white py-4 z-50">
+      <div className="flex justify-between mx-auto w-full max-w-[1300px] ">
+        <div className="flex gap-2 cursor-pointer">
+          <div>
+            <Image
+              src="/assets/searchIcon.webp"
+              alt="Vercel Logo"
+              width={32}
+              height={32}
+              priority
+            />
+          </div>
+          <h1 className="text-[1.5rem] font-semibold mb-4">Bangkok Asset</h1>
         </div>
-
-        <h1 className="text-[21.69px] font-semibold mb-4">Bangkok Asset</h1>
-      </div>
-      <div className="flex m-auto">
-        <button className="bg-[#fd5631] hover:bg-[#fd390e] hover:shadow-lg hover:shadow-[#fd5631]/30 text-white font-medium py-[6.8px] px-4 rounded transition-all duration-200 ease-in-out flex items-center justify-center">
+        <div className="flex bg-[#fd5631] hover:bg-[#fd390e] cursor-pointer hover:shadow-lg hover:shadow-[#fd5631]/30 text-white font-medium px-4 rounded transition-all duration-200 ease-in-out items-center justify-center">
           <svg
             className="mr-2 -ml-1 w-4 h-4"
             fill="none"
@@ -33,8 +31,8 @@ export default function NavBar() {
               d="M12 4v16m8-8H4"
             ></path>
           </svg>
-          Add Property
-        </button>
+          <p>Addproperty</p>
+        </div>
       </div>
     </div>
   );
