89b10f2c02597516a9395a9f380ed09258a8456d
nata-non <natanon.han@gmail.com>
2024-03-24 22:25:13 +0700

    feat: back to top button component

---
+++ src/components/toTopButton.tsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import { FaArrowUp } from "react-icons/fa";
+
+export default function BackToTopButton() {
+  const [isVisible, setIsVisible] = useState(false);
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
+
+  return (
+    <>
+      {isVisible && (
+        <button
+          onClick={scrollToTop}
+          className="fixed bottom-10 right-10 bg-white p-2 rounded-full shadow-lg cursor-pointer hover:bg-gray-100 z-50"
+          aria-label="Scroll to top"
+        >
+          <FaArrowUp />
+        </button>
+      )}
+    </>
+  );
+}
