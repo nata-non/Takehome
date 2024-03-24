ab68915834e6f35ec5837c9a1f5d79f0cc9dd2e2
nata-non <natanon.han@gmail.com>
2024-03-24 21:48:00 +0700

    feat: section 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import NavBar from "../components/navBar";
 import Carousel from "@/components/carousel";
 import HeroSection from "@/components/herosection";
+import Slider from "@/components/section4";
 
 export default function Home() {
   return (
@@ -10,9 +11,8 @@ export default function Home() {
       <NavBar />
       <HeroSection />
       <Carousel />
-      <div className="flex items-center justify-center w-full h-96 bg-fixed bg-cover">
-        <h1>Hello</h1>
-      </div>
+
+      <Slider />
       <div className="flex items-center justify-center w-full h-96 bg-fixed bg-cover">
         <h1>Hello</h1>
       </div>
